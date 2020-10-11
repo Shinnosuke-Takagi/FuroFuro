71a96a1458100f6d1e1c6fdbc6549fbf10b5e5c1
ShinnosukeTakagi <s.takagi5664@gmail.com>
2020-10-12 08:33:11 +0900

    HashTag fix UI 3rd

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
   <div class="container">
+    <div class="card aqua-gradient mb-3">
+      <div class="card-header text-white text-center">{{ $tag->hashtag }}</div>
+    </div>
     <div class="card-deck">
-      <div class="card aqua-gradient">
-        <div class="card-header text-white text-center">{{ $tag->hashtag }}</div>
-      </div>
       @foreach($tag->articles as $article)
         <div class="card">
           <a href="{{ route('articles.show', ['article' => $article]) }}">
