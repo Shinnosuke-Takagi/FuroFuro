529b63a0893f6d60bc324a4276d7a54681941925
ShinnosukeTakagi <s.takagi5664@gmail.com>
2020-10-17 14:49:25 +0900

    Add Change Password

--- resources/views/users/accountEdit.blade.php
+++ resources/views/users/accountEdit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header aqua-gradient text-white">プロフィールの変更</div>
+                <div class="card-header aqua-gradient text-white">アカウント情報の変更</div>
 
                 <div class="card-body">
                   @if($errors->any())
@@ -21,7 +21,7 @@
                       @csrf
 
                       <div class="form-group row">
-                          <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
+                          <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレスの変更</label>
 
                           <div class="col-md-6">
                               <input id="email" type="email" class="form-control" name="email" value="{{ $auth_user->email ?? old('email') }}">
@@ -42,10 +42,18 @@
                       @csrf
 
                       <div class="form-group row">
-                          <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
+                          <label for="password" class="col-md-4 col-form-label text-md-right">パスワードの変更</label>
 
                           <div class="col-md-6">
-                              <input id="password" type="password" class="form-control" name="password" value="********">
+                              <input id="password" type="password" class="form-control" name="password">
+                          </div>
+                      </div>
+
+                      <div class="form-group row">
+                          <label for="password-confirm" class="col-md-4 col-form-label text-md-right">パスワードの変更（確認用）</label>
+
+                          <div class="col-md-6">
+                              <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                           </div>
                       </div>
 
