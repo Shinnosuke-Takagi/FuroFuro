ad78bfca049b70d1177322841495304254f0fc3f
ShinnosukeTakagi <s.takagi5664@gmail.com>
2020-10-21 04:48:28 +0900

    Add Error Page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card text-center">
+                  <div class="card-header aqua-gradient text-white">お探しのページは見つかりませんでした</div>
+
+                  <div class="card-body">
+                      <p>お手数ですが左上のロゴからトップページへお戻りください。</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+@endsection
