5315a4933f59b8ef62295d8014b905c053113e51
ShinnosukeTakagi <s.takagi5664@gmail.com>
2020-12-17 17:45:01 +0900

    feat Eager Load Fix

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $with = [
+      'user', 'replies.user'
+    ];
+
     public function user()
     {
         return $this->belongsTo('App\User');
